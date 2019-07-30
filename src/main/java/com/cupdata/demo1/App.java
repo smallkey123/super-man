fa8a46a1d4d8f18a620044469cbb88fbacd2ac4b
wangzhen <976101739@qq.com>
2019-07-30 15:06:39 +0800

    第一次提交测试

---
+++ src/main/java/com/cupdata/demo1/App.java
@@ -0,0 +1,15 @@
+package com.cupdata.demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		System.out.println("Hello World4");
+		System.out.println("Hello World5");
+	}
+}
